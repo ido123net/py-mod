549b34bec247ea5e26a52dcf9caf6cd8cc5db805
Ido Frenkel <Ido123net@gmail.com>
2022-10-15 22:57:29 +0300

    initial commit

---
+++ py_mod/__main__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from py_mod.main import main
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ py_mod/main.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    return 0
