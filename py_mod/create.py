1fb0da59ce7bc16caf4c697b07ef99287e7973ff
Ido Frenkel <Ido123net@gmail.com>
2022-10-16 00:11:10 +0300

    Make dir and __init__ file inside

--- py_mod/create.py
+++ py_mod/create.py
@@ -1,6 +1,15 @@
 from __future__ import annotations
 
+import os
+import pathlib
+
 
 def create(module: str) -> int:
-    print(module)
+    module_path = pathlib.Path(module)
+    os.mkdir(module_path)
+
+    _init_file = module_path / '__init__.py'
+    with open(_init_file, 'w'):
+        pass
+
     return 0
